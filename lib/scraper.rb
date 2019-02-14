68401f69f0c07090e86d2d6ab5a3617366c2e97b
jdm79 <james.malvern@gmail.com>
2019-02-14 00:02:49 +0000

    test: writes test to check truthiness of detail_scraper, to check that events is an array, adds passing code

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,22 @@
+require "nokogiri"
+require "json"
+require "httparty"
+
 class ConcertScraper
+  attr_reader :events
+
+  def initialize
+    @events = []
+  end
+
   def url_scraper
+    url = "http://www.wegottickets.com/searchresults/all"
+    unparsed_page = HTTParty.get(url)
+    parsed_page = Nokogiri::HTML(unparsed_page)
+  end
+
+  def detail_scraper
     true
   end
+
 end
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,8 +1,19 @@
 require 'scraper'
 
 RSpec.describe ConcertScraper do
+  
   it "should have a url scraper method" do
     scraper_object = ConcertScraper.new
     expect(scraper_object.url_scraper).to be_truthy
+  end
+
+  it "should have a detail scraper method" do
+    scraper_object = ConcertScraper.new
+    expect(scraper_object.detail_scraper).to be_truthy
+  end
+
+  it "should have an events method that returns an events array containing ticket data" do
+    scraper_object = ConcertScraper.new
+    expect(scraper_object.events).to be_instance_of Array
   end
 end
