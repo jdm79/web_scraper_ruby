e7ac9f008624d97fa260aef658f5cbc327c9bc38
jdm79 <james.malvern@gmail.com>
2019-02-14 00:09:59 +0000

    feature: maps over the data on wegottickets and saves the urls to detail_urls array

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,9 +3,10 @@ require 'json'
 require 'httparty'
 
 class ConcertScraper
-  attr_reader :events
+  attr_reader :detail_urls, :events
 
   def initialize
+    @detail_urls = []
     @events = []
   end
 
@@ -13,6 +14,11 @@ class ConcertScraper
     url = 'http://www.wegottickets.com/searchresults/all'
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
+
+    parsed_page.css('div.content.block-group.chatterbox-margin a').map { |link| 
+      @detail_urls << link[:href]
+    }
+    @detail_urls
   end
 
   def detail_scraper
