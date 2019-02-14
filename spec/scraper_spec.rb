a4737d9f7d17afcb656630d1cf4e414d65f757d3
jdm79 <james.malvern@gmail.com>
2019-02-14 01:00:54 +0000

    test: adds tests to cover the private methods which format and grab the data for detail page

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -27,4 +27,78 @@ RSpec.describe ConcertScraper do
       expect(scraper_object.detail_scraper).to be_instance_of Array
     end
   end
+
+  context "when accessing event data in ConcertScraper class" do
+    it "the events array should contain hashes" do
+      scraper_object = ConcertScraper.new
+      scraper_object.url_scraper
+      scraper_object.detail_scraper
+      expect(scraper_object.events.sample).to be_instance_of Hash
+    end
+
+    it "the event hash should have 6 keys" do
+      scraper_object = ConcertScraper.new
+      scraper_object.url_scraper
+      scraper_object.detail_scraper
+      random_event_hash = scraper_object.events.sample
+      expect(random_event_hash.keys.count).to eq 6
+    end
+  end
+
+  context "when looking at the keys and values in event hash" do
+    def create_service_object
+      scraper_object = ConcertScraper.new
+      scraper_object.url_scraper
+      scraper_object.detail_scraper
+    end
+    
+    it "should have a title" do
+      scraper_object = ConcertScraper.new
+      scraper_object.url_scraper
+      scraper_object.detail_scraper
+      random_event_hash = scraper_object.events.sample
+      expect(random_event_hash.has_key?(:title)).to be true
+    end
+
+    it "should have an artist" do
+      scraper_object = ConcertScraper.new
+      scraper_object.url_scraper
+      scraper_object.detail_scraper
+      random_event_hash = scraper_object.events.sample
+      expect(random_event_hash.has_key?(:artist)).to be true
+    end
+
+    it "should have a city" do
+      scraper_object = ConcertScraper.new
+      scraper_object.url_scraper
+      scraper_object.detail_scraper        
+      random_event_hash = scraper_object.events.sample
+      expect(random_event_hash.has_key?(:city)).to be true
+    end
+
+    it "should have a venue" do
+      scraper_object = ConcertScraper.new
+      scraper_object.url_scraper
+      scraper_object.detail_scraper
+      random_event_hash = scraper_object.events.sample
+      expect(random_event_hash.has_key?(:venue)).to be true
+    end
+
+    it "should have a date" do
+      scraper_object = ConcertScraper.new
+      scraper_object.url_scraper
+      scraper_object.detail_scraper
+      random_event_hash = scraper_object.events.sample
+      expect(random_event_hash.has_key?(:date)).to be true
+    end
+
+    it "should have a price" do
+      scraper_object = ConcertScraper.new
+      scraper_object.url_scraper
+      scraper_object.detail_scraper
+      random_event_hash = scraper_object.events.sample
+      expect(random_event_hash.has_key?(:price)).to be true
+    end
+  end
+
 end
